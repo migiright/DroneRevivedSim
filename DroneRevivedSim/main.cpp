503b03aed8a9e6aa247ebc9ad0c058d94259f5cd
migiright <migigit@yahoo.co.jp>
2017-10-18 15:27:30 +0900

    add MyMath.hpp v1.1

--- DroneRevivedSim/main.cpp
+++ DroneRevivedSim/main.cpp
@@ -1,8 +1,11 @@
 ﻿#include <iostream>
+#include "MyMath.hpp"
 
 using namespace std;
+using namespace MyMath;
 
 int main() {
-	cout << "Hello" << endl;
+	Vector<2> x{1.0, 2.0}, y{3.0, 4.0};
+	cout << (x+y).toString() << endl;
 	return 0;
 }
