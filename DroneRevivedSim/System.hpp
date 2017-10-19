299824e2ad48e9b2ed673c57ad3409de5d29996a
migiright <migigit@yahoo.co.jp>
2017-10-20 03:35:43 +0900

    add CommonSystem

--- DroneRevivedSim/System.hpp
+++ DroneRevivedSim/System.hpp
@@ -1,6 +1,7 @@
 ﻿#pragma once
 
 #include "MyMath.hpp"
+#include <functional>
 
 template<size_t StateD, size_t InputD>
 class System {
@@ -15,4 +16,31 @@ public:
 	virtual State f(const State &state, const Input &input) = 0;
 	virtual Input u(const State &state) = 0;
 	virtual State normalize(const State &state) { return state; }
+};
+
+template<size_t StateD, size_t InputD>
+class CommonSystem : public System<StateD, InputD> {
+public:
+	CommonSystem(
+		std::function<State(const State&, const Input&)> f,
+		std::function<Input(const State&)> u,
+		std::function<State(const State&)> normalize = [](const State& state) { return state; }
+	)
+		: f_(f), u_(u), normalize_(normalize)
+	{}
+
+	State f(const State &state, const Input &input) override {
+		return f_(state, input);
+	}
+	Input u(const State &state) override {
+		return u_(state);
+	}
+	State normalize(const State &state) override {
+		return normalize_(state);
+	}
+
+private:
+	std::function<State(const State&, const Input&)> f_;
+	std::function<Input(const State&)> u_;
+	std::function<State(const State&)> normalize_;
 };
