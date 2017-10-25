c4c01bb94a65fd26a1309c23ffa0336a82db674e
migiright <migigit@yahoo.co.jp>
2017-10-25 20:38:40 +0900

    change Mymath.hpp to v1.4

--- DroneRevivedSim/main.cpp
+++ DroneRevivedSim/main.cpp
@@ -1,27 +1,8 @@
 ﻿#include <iostream>
 #include "MatlabUtilitySample.h"
-#include "Math2.hpp"
 
 using namespace std;
-using namespace MyMath;
-
-double v(Vector<3> x)
-{
-	return x[0] * x[1]*x[1] * x[2]*x[2]*x[2];
-}
-
-auto f(Vector<3> x)
-{
-	return Vector<3>{x[0], x[1], x[2]};
-}
-
-auto g(Vector<3> x)
-{
-	return Matrix<3, 2>{x[0], x[1], x[1], x[2], x[2], x[0]};
-}
 
 int main() {
-	cout << lie(f, v, Vector<3>{2.0, 3.0, 5.0}) << endl;
-	cout << lie(g, v, Vector<3>{2.0, 3.0, 5.0}).toString() << endl;
 	return 0;
 }
