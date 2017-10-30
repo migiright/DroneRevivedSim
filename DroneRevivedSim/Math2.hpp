1a810992815b29a4d382e8dbfdaa9ea172351393
migiright <migigit@yahoo.co.jp>
2017-10-30 22:53:57 +0900

    modify calcMinimum()でシミュレーションのデータは取らないように

--- DroneRevivedSim/Math2.hpp
+++ DroneRevivedSim/Math2.hpp
@@ -5,6 +5,7 @@
 #include "System.hpp"
 #include "Simulator.hpp"
 #include "Algorithms.hpp"
+#include <iostream>
 
 constexpr double Pi = 3.14159265358979323846;
 
@@ -16,7 +17,7 @@ auto calcMinimum(Function function, MyMath::Vector<Dimension> initial)
 	using System = CommonSystem<Dimension, 0>;
 	auto system = std::make_shared<System>([function](const State &s, const Input &) { return -MyMath::jacobianVector(function, s); },
 		[](const State &s) { return Input{}; });
-	Simulator<System> simulator(system, initial, createRungeKutta<System>(1e-3));
+	Simulator<System> simulator(system, initial, createRungeKutta<System>(1e-3), nullptr, false);
 	State error;
 	simulator.simulateToConverge(1e-7);
 	auto minx = simulator.data().back().state;
