b2dfce3d0576c1394d0a6b408fc9a15bb43fc820
migiright <migigit@yahoo.co.jp>
2017-11-03 21:10:21 +0900

    add シミュレーション

--- DroneRevivedSim/main.cpp
+++ DroneRevivedSim/main.cpp
@@ -1,11 +1,26 @@
-#include <iostream>
-#include "Math2Sample.h"
-
+﻿#include <iostream>
+#include "DroneSystem.hpp"
+#include "Simulator.hpp"
+#include "MatlabUtility.hpp"
+#include "Algorithms.hpp"
+#include "SystemPlotter.hpp"
 
 using namespace std;
+using namespace MyMath;
+using namespace Drone;
 
 int main()
 {
-	calcMinimumSample();
+	auto system = make_shared<DroneSystem>(MyMath::Vector<2>{1.0, 1.0});
+	Simulator<DroneSystem> simulator(system, Vector<6>{0.9, 0.0, 0.0, 0.0, 0.0, 0.0},
+		createRungeKutta<DroneSystem>(1e-5), &cout, true, 1e-1);
+	simulator.simulateTo(10);
+
+	auto mu = make_shared<MatlabUtility>("result.m");
+	auto data = make_shared<Simulator<DroneSystem>::Data>(simulator.data());
+
+	SystemPlotter<DroneSystem> pl(mu, data);
+	pl.plotState();
+	pl.plotInput();
 	return 0;
 }
